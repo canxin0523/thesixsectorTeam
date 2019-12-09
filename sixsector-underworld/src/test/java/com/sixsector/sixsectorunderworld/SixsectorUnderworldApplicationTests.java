af9722fa4b377ec92b3d8a1c539fc70715556832
zhangmf <canxin0523@163.com>
2019-12-09 13:52:14 +0800

    框架提交

---
+++ sixsector-underworld/src/test/java/com/sixsector/sixsectorunderworld/SixsectorUnderworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixsector.sixsectorunderworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixsectorUnderworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
