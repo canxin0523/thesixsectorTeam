212eaefb6e70d993788826eac0a871be42f4a9bf
lmmz <lmmz@promote.cache-dns.local>
2020-03-06 19:25:24 +0800

    update

--- six-sector-front/src/systemPage/home/index.vue
+++ six-sector-front/src/systemPage/home/index.vue
@@ -1,25 +1,29 @@
 <template>
-  <a-layout id="homeLayout" :style="{ overflow: 'auto', height: '100vh' }">
-    <a-layout-sider :trigger="null" collapsible v-model="collapsed">
-      <a-menu theme="dark" mode="inline">
-        <a-menu-item>首页</a-menu-item>
-        <a-sub-menu v-for="(itParent,index) in NavData" :key="index" :title="itParent.title">
-          <template v-if="itParent.child">
-            <a-menu-item v-for="itChild in itParent.child" :key="itChild.navId">{{itChild.title}}</a-menu-item>
-          </template>
+  <a-layout id="home-layout" style="min-height: 100vh">
+    <a-layout-sider>
+      <div class="logo">地府管理系统</div>
+      <a-menu theme="dark" :defaultSelectedKeys="['1']" mode="inline">
+        <a-sub-menu
+          v-for="(item,index) in NavData"
+          :key="item.navId"
+          :class="{'disapper-arrow': (item.child.length == 0)}"
+        >
+          <div slot="title" class="title">
+            <img :src="'../../../static/images/home/home-icon'+ (index + 1) +'.png'" alt />
+            <span>{{item.title}}</span>
+          </div>
+          <a-menu-item
+            v-show="item.child.length"
+            v-for="child in item.child"
+            :key="child.navId"
+          >{{child.title}}</a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
     <a-layout>
-      <a-layout-header id="hell_head">
-        <a-icon
-          style=" margin-left: 15px;font-size: 1.2rem;vertical-align: middle;"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="()=> collapsed = !collapsed"
-        />
-      </a-layout-header>
-      <a-layout-content id="hell_content">
-        <router-view></router-view>
+      <a-layout-header style="background: #fff; padding: 0" />
+      <a-layout-content style="margin: 0 16px">
+        <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">Bill is a cat.</div>
       </a-layout-content>
     </a-layout>
   </a-layout>
@@ -29,65 +33,33 @@ export default {
   name: "home",
   data() {
     return {
-      collapsed: false,
       NavData: [
         // 菜单列表，待修改
         {
           navId: 100,
-          icons: "dashboard",
-          title: "生死簿",
-          path: "",
-          child: [
-            {
-              navId: 101,
-              icons: "",
-              title: "用户管理",
-              path: "/userInfo"
-            },
-            {
-              navId: 102,
-              icons: "",
-              title: "数据同步",
-              path: "/dataSynchroinsm"
-            }
-          ]
-        },
-        {
-          navId: 200,
-          icons: "audit",
-          title: "勾魂管理",
-          path: "",
+          title: "首页",
           child: []
         },
         {
-          navId: 300,
-          icons: "audit",
-          title: "十八层地狱",
-          path: "",
+          navId: 200,
+          title: "生死簿",
           child: [
             {
-              navId: 301,
-              icons: "",
-              title: "设备管理",
-              path: "/equipmentManage"
+              navId: 201,
+              title: "用户管理"
             },
             {
-              navId: 302,
-              icons: "",
-              title: "作业流程",
-              path: "/process"
-            },
-            {
-              navId: 303,
-              icons: "",
-              title: "用户管理",
-              path: "/hellUserManage"
+              navId: 202,
+              title: "数据同步"
             }
           ]
+        },
+        {
+          navId: 300,
+          title: "勾魂管理",
+          child: []
         }
-      ],
-      currentSelectChild: "",
-      currentParent: []
+      ]
     };
   },
   methods: {}
@@ -97,18 +69,68 @@ export default {
 @import "../../assets/css/common.less";
 </style>
 <style lang="less" scoped>
-#homeLayout {
-  #hell_head {
-    background: #fff;
-    padding: 0;
-    height: 7vh;
-  }
+#home-layout {
+  .ant-layout-sider {
+    flex: none !important;
+    width: 180px !important;
+    min-width: 180px !important;
+    max-width: 180px !important;
+    .ant-layout-sider-children {
+      .logo {
+        height: 48px;
+        line-height: 48px;
+        font-size: 14px;
+        color: #fff;
+        padding-left: 15px;
+        font-weight: 700;
+      }
+      .ant-menu {
+        li {
+          min-height: 50px;
+          /deep/ .ant-menu-submenu-title {
+            padding-left: 15px !important;
+            .title {
+              img {
+                width: 15px;
+                height: 15px;
+              }
+              span {
+                font-size: 14px;
+                color: #fff;
+                vertical-align: middle;
+              }
+            }
+          }
 
-  #hell_content {
-    margin: 1.5vh 10px;
-    padding: 10px;
-    background: #fff;
-    height: 90vh;
+          &.disapper-arrow {
+            /deep/ .ant-menu-submenu-title {
+              .ant-menu-submenu-arrow {
+                &::before {
+                  width: 0;
+                }
+                &::after {
+                  width: 0;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .ant-layout {
+    #hell_head {
+      background: #fff;
+      padding: 0;
+      height: 7vh;
+    }
+
+    #hell_content {
+      margin: 1.5vh 10px;
+      padding: 10px;
+      background: #fff;
+      height: 90vh;
+    }
   }
 }
 </style>
