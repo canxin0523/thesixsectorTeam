bc02b1a7251c4048cf429574ca734943d4e5f389
cnmazehao <cnmazehao@outlook.com>
2020-08-03 18:26:02 +0800

    新增了背景图，使其更符合主题

--- frontend/src/views/login/Common.vue
+++ frontend/src/views/login/Common.vue
@@ -3,7 +3,7 @@
     <div class="content">
       <div class="top">
         <div class="header">
-          <img alt="logo" class="logo" src="static/img/logo.png" />
+          <img alt="logo" class="logo" src="static/img/logo.png"/>
           <span class="title">{{systemName}}</span>
         </div>
         <div class="desc"></div>
@@ -49,13 +49,15 @@ export default {
     flex-direction: column;
     height: 100vh;
     overflow: auto;
-    background: #f0f2f5 url('image/CommonImage.jpg') no-repeat;
-    background-size: cover;
+    background: #f0f2f5 url('image/background.jpg') no-repeat;
+    background-size: 100% 100%;
     .content {
-      padding: 32px 0;
+      padding: 300px 0;
       flex: 1;
       @media (min-width: 768px){
-        padding: 116px 0 10px;
+        padding-top: 300px;
+        padding-right: 900px;
+        padding-bottom: 10px;
       }
       .top {
         text-align: center;
@@ -67,7 +69,7 @@ export default {
           }
           .logo {
             width: 40px;
-            height: 19px;
+            height: 30px;
             vertical-align: center;
             margin-right: 16px;
           }
--- frontend/src/views/login/Regist.vue
+++ frontend/src/views/login/Regist.vue
@@ -24,45 +24,6 @@
 
         <a-input size="large" type="password" autocomplete="false" placeholder="确认密码" v-decorator="['password2',{rules: [{ required: true, message: '至少6位密码' }, { validator: this.handlePasswordCheck }], validateTrigger: ['change', 'blur']}]"></a-input>
       </a-form-item>
-      <!--
-      <a-form-item
-        fieldDecoratorId="mobile"
-        :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入正确的手机号', pattern: /^1[3456789]\d{9}$/ }, { validator: this.handlePhoneCheck } ], validateTrigger: ['change', 'blur'] }">
-        <a-input size="large" placeholder="11 位手机号">
-          <a-select slot="addonBefore" size="large" defaultValue="+86">
-            <a-select-option value="+86">+86</a-select-option>
-            <a-select-option value="+87">+87</a-select-option>
-          </a-select>
-        </a-input>
-      </a-form-item>
-      <a-input-group size="large" compact>
-            <a-select style="width: 20%" size="large" defaultValue="+86">
-              <a-select-option value="+86">+86</a-select-option>
-              <a-select-option value="+87">+87</a-select-option>
-            </a-select>
-            <a-input style="width: 80%" size="large" placeholder="11 位手机号"></a-input>
-          </a-input-group>
-
-      <a-row :gutter="16">
-        <a-col class="gutter-row" :span="16">
-          <a-form-item
-            fieldDecoratorId="captcha"
-            :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入验证码' }], validateTrigger: 'blur'}">
-            <a-input size="large" type="text" placeholder="验证码">
-              <a-icon slot="prefix" type='mail' :style="{ color: 'rgba(0,0,0,.25)' }"/>
-            </a-input>
-          </a-form-item>
-        </a-col>
-        <a-col class="gutter-row" :span="8">
-          <a-button
-            class="getCaptcha"
-            size="large"
-            :disabled="state.smsSendBtn"
-            @click.stop.prevent="getCaptcha"
-            v-text="!state.smsSendBtn && '获取验证码'||(state.time+' s')"></a-button>
-        </a-col>
-      </a-row>
--->
       <a-form-item>
         <a-button
           size="large"
